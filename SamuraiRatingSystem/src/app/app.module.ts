bcde3a781ec66efd7d4a87c6679ca778fde12516
Jean-Pierre <Olwagenjp@gmail.com>
2018-02-04 20:19:39 +0200

    Finished the styling for the dashboard

--- SamuraiRatingSystem/src/app/app.module.ts
+++ SamuraiRatingSystem/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LogInComponent } from './components/log-in/log-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    DashboardComponent,
+    NavComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
---
+++ SamuraiRatingSystem/src/app/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css', '../../assets/main-style.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
