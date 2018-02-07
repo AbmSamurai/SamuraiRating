d1170e67413ee64c181b34ce0b0b04bb7c2555e8
Jean-Pierre <Olwagenjp@gmail.com>
2018-02-07 13:10:35 +0200

    Fixed Develop

--- SamuraiRatingSystem/src/app/app-routing.module.ts
+++ SamuraiRatingSystem/src/app/app-routing.module.ts
@@ -1,41 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { TeamViewComponent } from './components/team-view/team-view.component';
-
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { LogInComponent } from "./components/log-in/log-in.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { RegistrationComponent } from "./components/registration/registration.component";
+import { TeamViewComponent } from "./components/team-view/team-view.component";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full"
   },
   {
-    path: 'login',
+    path: "login",
     component: LogInComponent
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     component: DashboardComponent
   },
   {
-    path: 'registration',
+    path: "registration",
     component: RegistrationComponent
   },
-
-    path: 'teamView',
-    component: TeamViewComponent,
-        // canActivate: [AuthGuard]
-
+  {
+    path: "teamView",
+    component: TeamViewComponent
+    // canActivate: [AuthGuard]
   }
-
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
