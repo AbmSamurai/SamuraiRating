405aee591d1ea7d6f7d6ab4f3901a7606b629242
Athandile <athandiletembile020@gmail.com>
2018-02-09 07:50:36 +0200

    Star Tally

--- SamuraiRatingSystem/src/app/components/team-review/team-review.component.ts
+++ SamuraiRatingSystem/src/app/components/team-review/team-review.component.ts
@@ -1,36 +1,48 @@
-import { Observable } from 'rxjs/Observable';
-import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup, FormControl, FormArray} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  FormArray
+} from "@angular/forms";
 @Component({
-  selector: 'app-team-review',
-  templateUrl: './team-review.component.html',
-  styleUrls: ['./team-review.component.css']
+  selector: "app-team-review",
+  templateUrl: "./team-review.component.html",
+  styleUrls: ["./team-review.component.css"]
 })
 export class TeamReviewComponent implements OnInit {
-  Questions: String[];
-
   ratingForm: FormGroup;
   rating = 0;
-  criteria = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  criteria = [1, 2, 3, 4, 5];
   Stars: FormArray = new FormArray([]);
-  starIds: Number[];
+  starIds: Number[] = [];
 
   constructor(private fb: FormBuilder) {
-    this.Questions = ['Hello', 'Hello'];
-
-    console.log(this.ratingForm);
+    /* This is collecting the */
     for (let i = 0; i < this.criteria.length; i++) {
       this.Stars.push(new FormControl(null, [Validators.required]));
-      this.starIds.push((Math.random() * (999 - 100 - 1))
+      this.starIds.push(Math.round(Math.random() * (999 - 100 - 1)));
     }
+    console.log(this.starIds);
+
     this.ratingForm = fb.group({
-      'comment': [null, Validators.compose([Validators.required, Validators.maxLength(140)])],
-      'stars': this.Stars
+      comment: [
+        null,
+        Validators.compose([Validators.required, Validators.maxLength(140)])
+      ],
+      stars: this.Stars
       // [null, Validators.compose([Validators.required])]
     });
-
   }
 
   ngOnInit() {}
 
+  submitRating(review) {
+    console.log(review);
+
+    const rating = review.stars.reduce((total, val) => total + val);
+    console.log(rating);
+    this.ratingForm.reset();
+  }
 }
